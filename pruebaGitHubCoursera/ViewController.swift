bcf47abb80408e81e1c9a8a6aa1368035d5e7312
yolandamartre <yolandamartre@hotmail.com>
2015-12-04 20:23:08 -0600

    Initial Commit

---
+++ pruebaGitHubCoursera/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  pruebaGitHubCoursera
+//
+//  Created by Yolanda Martínez on 12/4/15.
+//  Copyright © 2015 Yolanda Martínez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
